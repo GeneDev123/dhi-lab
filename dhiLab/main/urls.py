384340d83c6887f9a571b63d46f47a5ffb339423
Eugene M. Reyes <eugenemreyes123@gmail.com>
2023-10-08 19:17:06 +0800

    Update the urls and the views.

--- dhiLab/main/urls.py
+++ dhiLab/main/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'), 
-  path('home/', views.home, name='home'),  
-  path('logout/', views.user_logout, name='logout'),
-  path('accounts/<str:login_or_register_param>/', views.user_login_and_register, name='login-register'),
+    path('accounts/<str:login_or_register_param>/', views.user_login_and_register, name='login-register'),
+    path('profile/', views.user_profile, name='user-profile'),
+    path('logout/', views.user_logout, name='logout'),
+    path('home/', views.home, name='home'),
+    path('', views.home, name='home'), 
+
+    path('api/items/', views.CustomUserList.as_view(), name='user-list'),
+    path('api/items/<int:pk>/', views.CustomUserDetail.as_view(), name='user-detail'),
 ]
--- dhiLab/main/views.py
+++ dhiLab/main/views.py
@@ -5,6 +5,10 @@ from django.contrib.auth import login, logout
 from .forms import CustomUserCreationForm
 from django.contrib.auth.decorators import login_required
 
+from rest_framework import generics
+from .models import CustomUser
+from .serializers import CustomUserSerializer
+
 def user_login_and_register(request, login_or_register_param):
   if request.user.is_authenticated:
     return redirect('home')
@@ -26,12 +30,26 @@ def user_login_and_register(request, login_or_register_param):
   else:
     form = CustomUserCreationForm if login_or_register_param == 'register' else AuthenticationForm()
 
-  return render(request, 'main/login-and-register.html', {'form': form})
+  return render(request, 'main/login-and-register.html', {'form': form, 'login_or_register': login_or_register_param})
 
 def user_logout(request):
   logout(request)
   return redirect('home') 
 
+class CustomUserList(generics.ListCreateAPIView):
+  queryset = CustomUser.objects.all()
+  serializer_class = CustomUserSerializer
+
+class CustomUserDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = CustomUser.objects.all()
+  serializer_class = CustomUserSerializer
+
+@login_required(login_url='/accounts/login/')
+def user_profile(request):
+  print("In profile view")
+  return render(request, 'main/profile.html')
+
 @login_required(login_url='/accounts/login/') 
 def home(request):
+  print("In home page")
   return render(request, 'main/home.html')
