0f299018743f7b8579869cc0de83db256f4779c5
Eugene M. Reyes <eugenemreyes123@gmail.com>
2023-10-23 05:06:35 +0800

    Added nltk download

--- dhiLab/main/chatbot_utils.py
+++ dhiLab/main/chatbot_utils.py
@@ -2,6 +2,8 @@ import json
 import random
 import numpy as np
 import nltk
+nltk.download('wordnet')
+nltk.download('punkt')
 from nltk.stem import WordNetLemmatizer
 from tensorflow.keras.models import load_model
 from tensorflow.keras.optimizers import SGD
