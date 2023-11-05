239b8417136d0a1d6a0307ebd92f6f9c8cba4555
Eugene M. Reyes <eugenemreyes123@gmail.com>
2023-11-05 18:55:41 +0800

    Updated the models.py and applied migrations

--- dhiLab/main/models.py
+++ dhiLab/main/models.py
@@ -29,5 +29,22 @@ class CustomUser(AbstractUser):
   is_having_hypertension = models.CharField(max_length=20, choices=PRESENT_CONDITION_OPTIONS, blank=True, null=True,)
   is_diabetic = models.CharField(max_length=20, choices=PRESENT_CONDITION_OPTIONS, blank=True, null=True,)  
 
+  is_health_care_professional = models.BooleanField(default=False, null=True, blank=True)
+  specialization = models.CharField(max_length=100, null=True, blank=True)
+  education = models.TextField(null=True, blank=True)
+  experience = models.PositiveIntegerField(null=True, blank=True)
+  board_certifications = models.CharField(max_length=200, null=True, blank=True)
+  professional_affiliations = models.TextField(null=True, blank=True)
+  hospital_affiliations = models.TextField(null=True, blank=True)
+  areas_of_expertise = models.TextField(null=True, blank=True)
+  patient_reviews_and_ratings = models.TextField(null=True, blank=True)
+  office_location = models.TextField(null=True, blank=True)
+  accepted_insurance_plans = models.TextField(null=True, blank=True)
+  publications_and_research = models.TextField(null=True, blank=True)
+  awards_and_recognitions = models.TextField(null=True, blank=True)
+  contact_phone = models.CharField(max_length=20, null=True, blank=True)
+  contact_address = models.TextField(null=True, blank=True)
+  contact_website = models.URLField(null=True, blank=True)
+
   def __str__(self):
     return f"{self.first_name} {self.last_name}"
